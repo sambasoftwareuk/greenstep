f7a46410c4dd513ac08bd15d88ca942e9085a23d
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2025-06-24 11:22:27 +0100

    big Card completed

---
+++ app/_molecules/bigCardWithImage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { CardImage } from "../_atoms/images";
+import { PrimaryButton } from "../_atoms/buttons";
+
+const BigCardWithImage = ({
+  title,
+  imageLink,
+  variant = 1,
+  buttonLabel = "INCELE",
+}) => {
+  const Title = (
+    <h2 className="text-2xl md:text-3xl font-bold text-blue-600 mb-4">
+      {title}
+    </h2>
+  );
+
+  const Image = (
+    <div className="overflow-hidden rounded-md my-5">
+      <div className="transition-transform duration-300 ease-in-out group-hover:scale-105">
+        <CardImage imageLink={imageLink} />
+      </div>
+    </div>
+  );
+
+  const Button = (
+    <div className="flex text-center justify-center mt-4">
+      <PrimaryButton label={buttonLabel} className="rounded-full" />
+    </div>
+  );
+
+  const variantMap = {
+    1: [Title, Image, Button],
+    2: [Image, Title, Button],
+    3: [Button, Image, Title],
+  };
+
+  const content = variantMap[variant] || variantMap[1];
+
+  return (
+    <div className="group bg-white flex flex-col rounded-lg shadow p-6 text-center justify-center my-6 transition-transform duration-300 hover:shadow-lg hover:scale-[1.02]">
+      {content}
+    </div>
+  );
+};
+
+export default BigCardWithImage;
--- app/molecule/page.jsx
+++ app/molecule/page.jsx
@@ -13,6 +13,7 @@ import { TestimonialCard } from "../_molecules/testimonialCard";
 import { LearnCard } from "../_molecules/learnCard";
 import { FAQSection } from "../_molecules/faqSection";
 import courses from "../mocks/courses.json";
+import CardWithBigImage from "../components/CardWithBigImage";
 
 const page = () => {
   return (
@@ -98,6 +99,7 @@ const page = () => {
         </ImageSlider>
       </div>
       <LearnCard />
+      <CardWithBigImage />
     </div>
   );
 };
