37c78d193461a2e55381c76a2ea102a518623d13
herrbaha <baha_i@hotmail.com>
2025-07-01 23:30:36 +0200

    fix reviews

--- app/_molecules/productBanner.jsx
+++ app/_molecules/productBanner.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import Link from "next/link";
-import bigCardProducts from "@/app/constants/bigCardProducts.json";
 
-const ProductBanner = () => {
-  const displayedProducts = bigCardProducts.slice(0, 4);
+const ProductBanner = ({bannerProducts}) => {
 
   return (
     <div className="bg-primary900 py-4 px-4 w-full flex justify-center">
-      <div className="w-10/12 grid grid-cols-1 md:grid-cols-4 gap-4">
-        {displayedProducts.map((product) => (
+      <div className="md:w-10/12 grid sm:grid-cols-2 grid-cols-1 md:grid-cols-4 gap-4">
+        {bannerProducts.map((product) => (
           <Link
             key={product.id}
             href={`/products/${product.id}`}
             className="cursor-pointer"
           >
-            <div className="bg-primary900 h-44 flex items-center justify-center ">
+            <div className="bg-primary900 h-44 flex items-center justify-center w-3/4">
               <div className="flex items-center space-x-4">
                 <h2 className="text-white text-sm font-semibold text-center">
                   {product.title}
