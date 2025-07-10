e629c6c4266ecbf3a32d8d7c3bf3f07be04efe9e
herrinanc <baha_i@hotmail.com>
2025-07-10 19:58:24 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,13 +5,10 @@ import sliderData from "./mocks/sliderData.json";
 import ProductBanner from "./_molecules/productBanner";
 import SliderComponent from "./_components/sliderComponent.jsx";
 import bannerProducts from "./constants/bannerProducts.json";
-<<<<<<< Updated upstream
 import CarouselSlider from "./_components/CarouselSlider.jsx";
-=======
 import BlogCard from "./_molecules/blogCard.jsx";
 import blogData from "./constants/blogData.json";
 import { Header3 } from "./_atoms/Headers.jsx";
->>>>>>> Stashed changes
 
 export default function Home() {
   return (
@@ -23,11 +20,7 @@ export default function Home() {
       />
       <ProductBanner bannerProducts={bannerProducts} />
       <MainProductComponent products={products} />
-<<<<<<< Updated upstream
       <CarouselSlider data={services} title="Hizmetlerimiz" isAutoSlide={true} isInfinite={true}  />
-      
-=======
-      <ProductCarousel products={products} title="Featured Products" />
       <div className="w-full flex flex-col items-center mt-8">
         <Header3 className="!text-black m-8">Blog</Header3>
         <div className="flex flex-wrap gap-6 justify-center">
@@ -44,15 +37,6 @@ export default function Home() {
           ))}
         </div>
       </div>
-      {/* <LearningPathwayComponent careers={careers} />
-      <MainTabComponent tabs={tabs} />
-      <ReferenceComponent referanceImages={referanceImages} />
-      <PopularContentsComponent courses={mockCourses} />
-      <GoalsComponent />
-      <TestimonialComponent testimonialData={testimonialData} />
-      <SkillsSection courses={mockCourses} />
-       */}
->>>>>>> Stashed changes
     </div>
   );
 }
