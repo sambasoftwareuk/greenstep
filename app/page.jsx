28968f64a8a25a42713728d1b7fb79f26e286e9d
herrbaha <baha_i@hotmail.com>
2025-07-16 12:16:06 +0200

    fix reviews

--- app/page.jsx
+++ app/page.jsx
@@ -6,9 +6,8 @@ import ProductBanner from "./_molecules/productBanner";
 import SliderComponent from "./_components/sliderComponent.jsx";
 import bannerProducts from "./constants/bannerProducts.json";
 import CarouselSlider from "./_components/CarouselSlider.jsx";
-import BlogCard from "./_molecules/blogCard.jsx";
 import blogData from "./constants/blogData.json";
-import { Header3 } from "./_atoms/Headers.jsx";
+import BlogCardList from "./_components/blogCardList.jsx";
 
 export default function Home() {
   return (
@@ -22,22 +21,7 @@ export default function Home() {
         isAutoSlide={true}
         isInfinite={true}
       />
-      <div className="w-full flex flex-col items-center mt-8">
-        <Header3 className="!text-black m-8">Blog</Header3>
-        <div className="flex flex-wrap gap-6 justify-center">
-          {blogData.map((post, index) => (
-            <BlogCard
-              key={index}
-              imageLink={post.imageLink}
-              imageAlt={post.imageAlt}
-              date={post.date}
-              title={post.title}
-              excerpt={post.excerpt}
-              aspectRatio={post.aspectRatio}
-            />
-          ))}
-        </div>
-      </div>
+     <BlogCardList blogData={blogData} />
     </div>
   );
 }
