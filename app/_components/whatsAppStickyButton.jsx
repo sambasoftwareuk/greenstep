f6b9b470d0c75f81c5e5c694856e8bd54fb0666e
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2025-07-18 19:38:04 +0100

    whatsapp icon name changed

--- app/_components/whatsAppStickyButton.jsx
+++ app/_components/whatsAppStickyButton.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { WhatsappIcon } from "../_atoms/Icons";
+import { WhatsAppIcon } from "../_atoms/Icons";
 import Icon from "../_atoms/Icon";
 import Link from "next/link";
 import contact from "../constants/contact.json";
 
-const WhatsaapStickyButton = () => {
+const WhatsAppStickyButton = () => {
   return (
     <Link
       href={contact.whatsappUrl}
       target="_blank"
       className="fixed bottom-6 right-4 flex items-center gap-2 bg-green-500 hover:bg-green-600 text-white px-3 py-3 rounded-xl md:rounded-full shadow-lg z-50 transition-all"
     >
-      <Icon variant={WhatsappIcon} size={36} color="text-white" />
+      <Icon variant={WhatsAppIcon} size={36} color="text-white" />
       <span className="hidden md:flex font-semibold text-lg">
         WhatsApp Destek Hattı
       </span>
@@ -19,4 +19,4 @@ const WhatsaapStickyButton = () => {
   );
 };
 
-export default WhatsaapStickyButton;
+export default WhatsAppStickyButton;
