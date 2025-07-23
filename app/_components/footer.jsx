3db592c581374eaec04fbbe0d33cc9aa01d23280
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2025-07-23 13:17:38 +0100

    footerBlog move into the footer

--- app/_components/footer.jsx
+++ app/_components/footer.jsx
@@ -2,8 +2,8 @@ import { SambaLinks } from "../_atoms/SambaLinks";
 import Icon from "../_atoms/Icon";
 import { Globe } from "../_atoms/Icons";
 import footerData from "../mocks/footerData";
-import { LogoImage } from "../_atoms/images";
 import { AccordionSection } from "../_molecules/accordionSection";
+import FooterBlogList from "./FooterBlogList";
 
 export function Footer() {
   return (
@@ -69,19 +69,19 @@ export function Footer() {
         </div>
       </div>
 
-      <div className="border-t border-white w-full" />
+      <FooterBlogList />
+      <div className="border-t border-black w-full" />
 
-      <div className="bg-gray-950 w-full text-sm text-white py-6 px-6">
+      <div className="bg-gray-200 w-full text-sm text-gray-800  py-6 px-6">
         <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-between items-center gap-4">
           <div className="flex items-center gap-2">
-            <LogoImage imageLink="/green-step-logo.png" />
             <span>
-              © {new Date().getFullYear()} SambaAcademy, Inc. All rights
-              reserved.
+              Greenstep Su Soğutma Kuleleri © {new Date().getFullYear()}{" "}
+              SambaAcademy, Inc. All rights reserved.
             </span>
           </div>
           <div className="flex items-center gap-1">
-            <Icon variant={Globe} size={12} color="text-white" />
+            <Icon variant={Globe} size={12} color="" />
             <span>English</span>
           </div>
         </div>
