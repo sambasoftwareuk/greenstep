cbcc96dc458b6dc113c2b94ad3d62f12a21175cf
herrbaha <baha_i@hotmail.com>
2025-07-29 22:54:13 +0200

    add breadcrumb blog

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,9 +1,11 @@
 import { Header1 } from "../_atoms/Headers";
 import BlogCard from "../_molecules/blogCard";
+import Breadcrumb from "../_molecules/breadCrumb";
 import blogData from "../mocks/blogData.json";
 
 const Blog = () => (
   <div className="w-full flex flex-col items-center mt-8">
+    <Breadcrumb/>
     <Header1 className="m-8">Blog</Header1>
     <div className="flex flex-wrap gap-6 justify-center mb-4">
       {blogData.map((post, index) => (
